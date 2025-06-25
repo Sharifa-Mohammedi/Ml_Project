20f810b605e556a6121ac557208c01f8f56b20d1
Sharifa munawar <Smunawar@ESTUATE.COM>
2025-06-25 16:29:47 +0530

    Data Ingestion

---
+++ src/Components/data_ingestion.py
@@ -0,0 +1,47 @@
+import os
+import sys
+from src.exception import CustomException
+from src.logger import logging
+import pandas    as pd
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+@dataclass
+class DataInjectionConfig:
+    train_data_path: str = os.path.join('aircrafts', "train.csv")
+    test_data_path: str = os.path.join('aircrafts', "test.csv")
+    raw_data_path: str = os.path.join('aircrafts', "data.csv")
+    
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config = DataInjectionConfig()
+        
+    def initiate_data_ingestion(self):# 4
+        logging.info("Entered the data ingestion method or component")
+        try:
+            df=pd.read_csv('Notebook\data\stud.csv')  #5
+            logging.info('Read the dataset as dataframe')
+            
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True) #6 saves the raw data to aircrafts file 
+            
+            logging.info("Train Test intiated")
+            train_set,test_set=train_test_split(df, test_size=0.2, random_state=42)# 7  Split the data into train and test
+            #80% goes to training and 20% to testing.
+            
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)#saves the train_data to aircrafts file 
+
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+            logging.info("Ingestion of data is completed")
+           
+            
+            return(
+                self.ingestion_config.train_data_path, #9 
+                self.ingestion_config.test_data_path
+            )
+        except Exception as e:
+            raise CustomException(e, sys)
+
+if __name__=='__main__': #1
+    obj=DataIngestion() #2
+    obj.initiate_data_ingestion() #3 actuall start
